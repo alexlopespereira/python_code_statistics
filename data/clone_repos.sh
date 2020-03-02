d7335221d1f7b08490f06db97b1de35563fd9ee5
Alex Pereira <alex.pereira@planejamento.gov.br>
2020-03-02 01:53:38 -0300

    fixes

--- data/clone_repos.sh
+++ data/clone_repos.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-
-curl -u alexlopespereira:b206a102b3b038aed0dd217bb4f0c5616bebf229 "https://api.github.com/user/repos?per_page=1000" | grep -e 'svn_url*' | cut -d \" -f 4 | xargs -L1 git clone
+USER=$1
+TOKEN=$2
+curl -u $USER:$TOKEN "https://api.github.com/user/repos?per_page=1000" | grep -e 'svn_url*' | cut -d \" -f 4 | xargs -L1 git clone
